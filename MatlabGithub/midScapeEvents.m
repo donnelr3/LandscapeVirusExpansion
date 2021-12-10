b6e89d513c30e5c9acd0b0a563fa29b8f2a540ed
donnelr3 <43070609+donnelr3@users.noreply.github.com>
2021-12-10 20:02:52 +0000

    Delete MatlabGithub directory

--- MatlabGithub/midScapeEvents.m
+++
@@ -1,6 +0,0 @@
-function [value,isterminal,direction] = midScapeEvents(t,y)
-sizy=size(y);
-numFields=sizy(1)/20;
-value = y(7*numFields+(numFields/2)-20)-10;     % Detect height = 0
-isterminal = 1;   % Stop the integration
-direction = 1;   % Negative direction only
